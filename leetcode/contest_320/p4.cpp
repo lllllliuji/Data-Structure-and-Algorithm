aab26025352669aae5ccf79e70ea5e0800920dbd
liuji <1546243245@qq.com>
2023-02-10 13:44:40 +0800

    update contest_320

--- leetcode/contest_320/p4.cpp
+++ leetcode/contest_320/p4.cpp
@@ -1,6 +1,6 @@
 class Solution {
    public:
-    bool is_prime(char x) { return x == '2' || x == '3' || x == '5' || x == '7'; }
+    bool is_prime(char x) { return x == '2' || x == '5' || x == '7' || x == '3'; }
     int beautifulPartitions(string s, int k, int minLength) {
         if (!is_prime(s[0])) {
             return 0;
@@ -11,14 +11,15 @@ class Solution {
         long g[n + 1][k + 1];
         memset(f, 0, sizeof f);
         memset(g, 0, sizeof g);
-        f[0][0] = g[0][0] = 1;
+        f[0][0] = 1;
+        for (int i = 0; i <= n; i++) g[i][0] = 1;
         for (int i = 0; i < n; i++) {
             if (i >= minLength - 1 && !is_prime(s[i]) && (i == n - 1 || is_prime(s[i + 1]))) {
                 for (int j = 1; j <= k; j++) {
                     f[i + 1][j] = g[i - minLength + 1][j - 1];
                 }
             }
-            for (int j = 0; j <= k; j++) {
+            for (int j = 1; j <= k; j++) {
                 g[i + 1][j] = (g[i][j] + f[i + 1][j]) % mod;
             }
         }
