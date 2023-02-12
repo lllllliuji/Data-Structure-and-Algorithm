d6dd89cd915e8689850ddeb852451e8e3ce4c8ca
liuji <1546243245@qq.com>
2023-02-12 14:43:20 +0800

    update contest_332

--- leetcode/contest_332/p2.cpp
+++ leetcode/contest_332/p2.cpp
@@ -7,9 +7,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             auto low = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]);
             auto high = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]);
-            if (low != nums.end()) {
-                ans += high - low;
-            }
+            ans += high - low;
         }
         return ans;
     }
